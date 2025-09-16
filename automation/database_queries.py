d1ca7ff698670257b0c78a582f5ea98e1fb8a1f4
riniangreani <rini.angreani@curtin.edu.au>
2025-09-16 14:09:55 +0800

    Added script to update and populate observation and tile tables, some 1d pipeline handling

--- automation/database_queries.py
+++ automation/database_queries.py
@@ -9,11 +9,11 @@ from datetime import datetime
 def execute_query(query, params=None):
     """
     Execute a SQL query and return the results.
-    
+
     Args:
     query (str): The SQL query to execute.
     params (tuple): Optional parameters for the SQL query.
-    
+
     Returns:
     list: The results of the query.
     """
@@ -26,28 +26,28 @@ def execute_query(query, params=None):
         'host': os.getenv('DATABASE_HOST'),
         'port': os.getenv('DATABASE_PORT')
     }
-    
+
     try:
         # Connect to the database
         conn = psycopg2.connect(**conn_params)
         cursor = conn.cursor()
-        
+
         # Execute the query
         if params:
             print(f"Executing database query: {query % params}")
         else:
             print(f"Executing database query: {query}")
         cursor.execute(query, params)
-        
+
         # Fetch all results
         results = []
         if cursor.description is not None:
             results = cursor.fetchall()
-        
+
         # Commit changes if it's an insert/update
-        if query.strip().lower().startswith(('insert', 'update', 'create')):
+        if query.strip().lower().startswith(('insert', 'update', 'create', 'alter')):
             conn.commit()
-        
+
         return results
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -58,7 +58,7 @@ def execute_query(query, params=None):
             cursor.close()
         if conn:
             conn.close()
-          
+
 def update_3d_pipeline_status(tile_number, band, status):
     """
     Update the 'tile' table, setting either '3d_pipeline_band1' or '3d_pipeline_band2'
@@ -67,7 +67,7 @@ def update_3d_pipeline_status(tile_number, band, status):
         - WaitingForValidation (Job has finished running successfully, waiting for human validation)
         - Good/Bad (Currently has to be manually set after human validation)
         - Failed (Job failed to run)
-    
+
     Args:
     tile_number (str): The tile number to update.
     band (str): The band of the tile.
@@ -75,6 +75,7 @@ def update_3d_pipeline_status(tile_number, band, status):
     """
     validate_band_number(band)
     column_name = "3d_pipeline_band2_status" if band == '2' else "3d_pipeline_band1_status"
+    print(f"Updating POSSUM tile database table with {column_name} to {status}")
     query = f"""
         UPDATE possum.tile
         SET "{column_name}" = %s
@@ -89,7 +90,7 @@ def update_1d_pipeline_validation_status(field_name, sbid, band_number, status):
     Update the 1d_pipeline_validation_status column in the observation table.
     This is to replace updated POSSUM pipeline validation Google sheet: Partial Tile Pipeline - regions - Band 1,
     1d_pipeline_validation column.
-    
+
     Args:
     field_name       : observation.field_name
     sbid             : observation.sbid
@@ -97,6 +98,7 @@ def update_1d_pipeline_validation_status(field_name, sbid, band_number, status):
     status (str): The status to set in the 'status_column' column.
 
     """
+    print("Updating POSSUM observation table with 1D pipeline summary plot status")
     validate_band_number(band_number)
     column_name = "1d_pipeline_validation_band2" if band_number == '2' else "1d_pipeline_validation_band1"
     query = f"""
@@ -107,100 +109,133 @@ def update_1d_pipeline_validation_status(field_name, sbid, band_number, status):
     params = (status, field_name, sbid)
     execute_query(query, params)
 
-def find_boundary_issues():
-    query = f"""
-        SELECT *
+def update_single_SB_1d_pipeline_status(field_name, sbid, band_number, status):
     """
-    band_number = '1' if band == '943MHz' else '2'
-    tile_sheet = ps.worksheet(f'Partial Tile Pipeline - regions - Band {band_number}')
-    tile_data = tile_sheet.get_all_values()
-    column_names = tile_data[0]
-    tile_table = at.Table(np.array(tile_data)[1:], names=column_names)
+    Update the single_1d_pipeline_validation{band_number} column in the observation table.
+    This is to the equivalent to POSSUM pipeline status sheet: Survey Fields - Band {band_number}
 
-    # Build field name prefix
-    fieldname = "EMU_" if band == '943MHz' else 'WALLABY_'  # TODO: verify WALLABY_ fieldname
-    full_field_name = f"{fieldname}{field_ID}"
+    Args:
+    field_name       : observation.field_name
+    sbid             : observation.sbid
+    band_number      : '1' or '2'
+    status (str): The status to set in the 'status_column' column.
 
-    # Update all rows belonging to field and sbid
-    rows_to_update = [
-        idx + 2  # +2 because gspread index is 1-based and skips the header row
-        for idx, row in enumerate(tile_table)
-        if row['field_name'] == full_field_name and row['sbid'] == str(SBid)
-    ]
+    """
+    validate_band_number(band_number)
+    column_name = "single_SB_1D_pipeline_band1" if band_number == '2' else "single_SB_1D_pipeline_band2"
+    print(f"Updating POSSUM observation table with {column_name} status")
+    query = f"""
+        UPDATE possum.observation
+        SET {column_name} = %s
+        WHERE field_name = %s AND sbid = %s;
+    """
+    params = (status, field_name, sbid)
+    execute_query(query, params)
 
-    if not rows_to_update:
-        print(f"No rows found for field {full_field_name} and SBID {SBid}")
-        return False
-    
-    # Determine the column letter of the status column.
-    # col_letter = gspread.utils.rowcol_to_a1(1, column_names.index(status_column) + 1)[0]
-    
-    # Determine the index (1-based) for the column to update
-    col_index = column_names.index(status_column) + 1
-    
-    # Prepare a list of cell updates.
-    cells = [Cell(r, col_index, status) for r in rows_to_update]
+def find_boundary_issues(sbid, observation):
+    """
+    Check if there are any entries in partial_tile_1d_pipeline for the given sbid and observation
+    where type indicates it crosses a projection boundary.
+    This is to identify potential issues with tiles that cross projection boundaries.
+    """
+    print(f"Checking for projection boundary issues for SBID: {sbid}, Observation: {observation}")
+    query = """
+        SELECT EXISTS (
+            SELECT 1
+            FROM possum.partial_tile_1d_pipeline
+            WHERE sbid = %s AND observation = %s AND type like '%%crosses projection boundary%%'
+        ) AS match_found;
+    """
+    params = (sbid, observation)
+    results = execute_query(query, params)
+    issues_found = results[0][0]
+    if issues_found:
+        print("Boundary issues found.")
+    else:
+        print("No boundary issues found.")
+    return issues_found
 
-    # Check if any row has "crosses projection boundary" in the type column
-    boundary_issue = False
-    for row_index in rows_to_update:
-        # Check for the projection boundary issue
-        if "crosses projection boundary" in tile_table['type'][row_index - 2].lower():
-            boundary_issue = True    
-    
 def validate_band_number(band_number):
     if band_number not in ["1", "2"]:
-        raise ValueError("band_number must be either 1 or 2")    
+        raise ValueError("band_number must be either 1 or 2")
 
 def get_tiles_for_pipeline_run(band_number):
     """
-    Get a list of tile numbers that should be ready to be processed by the 3D pipeline 
-    
+    Get a list of tile numbers that should be ready to be processed by the 3D pipeline
+
     In the database, this is when:
     observation.cube_state = 'COMPLETED' and tile.3d_pipeline_band{band_number}_status = [null]
     In POSSUM pipeline status sheet, this is the equivalent of:
     'aus_src' column is not empty and '3d_pipeline' column is empty for the given band number.
-    
+
     Args:
     band_number (int): The band number (1 or 2) to check.
-    
+
     Returns:
     list: A list of tile numbers that satisfy the conditions.
     """
     validate_band_number(band_number)
     column_name = "3d_pipeline_band2_status" if band_number == 2 else "3d_pipeline_band1_status"
+    print(f"Fetching tiles ready for 3D pipeline run for band {band_number} from the database.")
     query = f"""
         SELECT DISTINCT tile.tile
-        FROM possum.tile 
-        INNER JOIN possum.associated_tile 
+        FROM possum.tile
+        INNER JOIN possum.associated_tile
         ON associated_tile.tile = tile.tile
-        INNER JOIN possum.observation 
+        INNER JOIN possum.observation
         ON associated_tile.name = observation.name
-        WHERE observation.cube_state = 'COMPLETED' AND tile."{column_name}" IS NULL;  
+        WHERE observation.cube_state = 'COMPLETED' AND tile."{column_name}" IS NULL;
     """
-    #TODO: update the table with existing values from google sheet    
+    #TODO: update the table with existing values from google sheet
     return execute_query(query)
-    
-def update_validation_in_db(field_ID, tile_numbers, band, status):
+
+def update_partial_tile_1d_pipeline_status(field_id, tile_numbers, band_number, status):
     """
-    Update the validation status of a tile in the database.
-    We don't need to do this for 3D because it's for manual validation.
-    
+    Update 1d_pipeline_band_{band_number} status for a set of tiles in the partial_tile_1d_pipeline table.
+    This replaces the 1d_pipeline column in the POSSUM pipeline validation Google sheet:
+    Partial Tile Pipeline - regions - Band {band_number}
+
     Args:
     field_ID (str): The field ID.
     tile_numbers (tuple): The tile numbers to update.
-    band (str): The band of the tile.
     status (str): The new validation status to set.
     """
+    print(f"Updating POSSUM partial_tile_1d_pipeline.1d_pipeline_band_{band_number} in the database")
     t1, t2, t3, t4 = tile_numbers
-    query = """
-        UPDATE validation_tiles
-        SET validation_status = %s
-        WHERE field_ID = %s AND tile1 = %s AND tile2 = %s AND tile3 = %s AND tile4 = %s AND band = %s;
+    query = f"""
+        UPDATE possum.partial_tile_1d_pipeline
+        SET 1d_pipeline_band{band_number} = %s
+        WHERE field_ID = %s AND tile1 = %s AND tile2 = %s AND tile3 = %s AND tile4 = %s;
     """
-    params = (status, field_ID, t1, t2, t3, t4, band)
-    execute_query(query, params)	
-    
+    params = (status, field_id, t1, t2, t3, t4)
+    return execute_query(query, params)
+
+def get_partial_tiles_for_1d_pipeline_run(band_number):
+    """
+    Get a list of partial tiles that should be ready to be processed by the 1D pipeline
+    In the database, this is when:
+    partial_tile_1d_pipeline.sbid is not NULL, number_sources is not NULL,
+    and 1d_pipeline_band_{band_number} is NULL
+
+    Args:
+    band_number (int): The band number (1 or 2) to check.
+
+    Returns:
+    list: A list of tuples containing (field_ID, sbid, (tile1, tile2, tile3, tile4))
+    that satisfy the conditions.
+    """
+    validate_band_number(band_number)
+    print(f"""Fetching partial tiles ready for 1D pipeline run for band {band_number}
+          from the database.""")
+    query = f"""
+        SELECT pt.observation, pt.sbid, pt.tile1, pt.tile2, pt.tile3, pt.tile4
+        FROM possum.partial_tile_1d_pipeline pt
+        WHERE pt.sbid IS NOT NULL
+          AND pt.number_sources IS NOT NULL
+          AND pt."1d_pipeline_band{band_number}" IS NULL;
+    """
+    return execute_query(query)
+
 #    def check_3d_pipeline_complete():
 #        SELECT *
 #FROM possum.tile
@@ -209,7 +244,7 @@ def update_validation_in_db(field_ID, tile_numbers, band, status):
 def update_aussrc_status(tile_number, band):
     """
     Update the status of a tile in the database.
-    
+
     Args:
     tile_number (str): The tile number to update.
     band (str): The band of the tile.
@@ -220,4 +255,4 @@ def update_aussrc_status(tile_number, band):
         WHERE tile = %s;
     """
     #make sure to do this to local copy
-    # au     
+    # au
--- automation/insert_database_script.py
+++ automation/insert_database_script.py
@@ -1,11 +1,15 @@
 """
 Handy scripts to update the database with the Google spreadsheets data.
+Usage: python3 insert_database_script.py
 """
 import os
 import gspread
 from dotenv import load_dotenv
 import database_queries as db
 
+gc = None
+validation_sheet = None
+status_sheet = None
 
 def create_partial_tile_pipeline_table():
     """
@@ -16,13 +20,14 @@ def create_partial_tile_pipeline_table():
             id SERIAL PRIMARY KEY,
             observation TEXT,
             sbid CHARACTER VARYING,
-            tile_1 BIGINT,
-            tile_2 BIGINT,
-            tile_3 BIGINT,
-            tile_4 BIGINT,
+            tile1 BIGINT,
+            tile2 BIGINT,
+            tile3 BIGINT,
+            tile4 BIGINT,
             type TEXT,
             number_sources INT,
-            "1d_pipeline" TEXT
+            "1d_pipeline_band1" TEXT,
+            "1d_pipeline_band2" TEXT
         );
     """
     db.execute_query(sql)
@@ -30,10 +35,6 @@ def create_partial_tile_pipeline_table():
 def insert_partial_tile_data():
     """Stream the Google Sheet into the database table
     """
-    load_dotenv(dotenv_path='config.env')
-    google_api_token = os.getenv('GOOGLE_API_TOKEN_PATH')
-    validation_sheet = os.getenv('POSSUM_PIPELINE_VALIDATION_SHEET')
-    gc = gspread.service_account(filename=google_api_token)
     ps = gc.open_by_url(validation_sheet)
     tile_sheet = ps.worksheet('Partial Tile Pipeline - regions - Band 1')
     tile_data = tile_sheet.get_all_values()
@@ -41,8 +42,8 @@ def insert_partial_tile_data():
     for row in tile_data[1:]:  # Skip header row
         sql = """
             INSERT INTO possum.partial_tile_1d_pipeline
-            (observation, sbid, tile_1, tile_2, tile_3, tile_4, type, number_sources, "1d_pipeline")
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            (observation, sbid, tile1, tile2, tile3, tile4, type, number_sources, "1d_pipeline_band1", "1d_pipeline_band2")
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
         args = (
            row[0],  # observation
@@ -53,13 +54,122 @@ def insert_partial_tile_data():
            row[5] if row[5].isdigit() else None,  # tile_4
            row[6],  # type
            row[7] if row[7].isdigit() else None,  # number_sources
-           row[8] if len(row) > 8 else None  # 1d_pipeline
+           row[8] if len(row) > 8 else None, # 1d_pipeline_band_1
+           None # id_pipeline_band_2
         )
         db.execute_query(sql, args)
 
-if __name__ == "__main__":
-    """Main function to create the table and insert data.
+def update_observation_table():
+    """Add additional columns to the observation table if they do not exist"""
+    add_column_sql = """
+        ALTER TABLE possum.observation
+        ADD COLUMN IF NOT EXISTS single_SB_1D_pipeline_band1 TEXT,
+        ADD COLUMN IF NOT EXISTS single_SB_1D_pipeline_band2 TEXT,
+        ADD COLUMN IF NOT EXISTS "1d_pipeline_validation_band1" TEXT,
+        ADD COLUMN IF NOT EXISTS "1d_pipeline_validation_band2" TEXT;
     """
+    db.execute_query(add_column_sql)
+    #POSSUM Status Sheet: Survey Fields - Band 1: single_SB_1D_pipeline
+    ps = gc.open_by_url(status_sheet)
+    tile_sheet = ps.worksheet('Survey Fields - Band 1')
+    tile_data = tile_sheet.get_all_values()
+    for row in tile_data[1:]:  # Skip header row
+        set_observation_single_SB_1D_pipeline(row, band_number='1')
+
+    #POSSUM Status Sheet: Survey Fields - Band 2: single_SB_1D_pipeline
+    tile_sheet = ps.worksheet('Survey Fields - Band 2')
+    tile_data = tile_sheet.get_all_values()
+    for row in tile_data[1:]:  # Skip header row
+        set_observation_single_SB_1D_pipeline(row, band_number='2')
+
+    #POSSUM Pipeline Validation: Partial Tile Pipeline - regions - Band 1: 1d_pipeline_validation
+    ps = gc.open_by_url(validation_sheet)
+    tile_sheet = ps.worksheet('Partial Tile Pipeline - regions - Band 1')
+    tile_data = tile_sheet.get_all_values()
+    for row in tile_data[1:]:  # Skip header row
+        if row[9] == '': # Skip empty cells
+            continue
+        #TODO: update when Erik replies re: 1d_pipeline_validation column
+        # For now, let's assume 1 observation = 1 1d_pipeline_validation status
+        sql = """
+            UPDATE possum.observation o
+            SET "1d_pipeline_validation_band1" = %s
+            FROM possum.partial_tile_1d_pipeline pt
+            WHERE o.name= %s and o.name = pt.observation
+            and pt.sbid = %s and pt.tile1 = %s
+            and pt.tile2 = %s and pt.tile3 = %s and pt.tile4 = %s;
+       """
+        args = (
+           row[9],  # 1d_pipeline_validation
+           row[0],  # observation
+           row[1],  # sbid
+           row[2] if row[2].isdigit() else None,  # tile_1
+           row[3] if row[3].isdigit() else None,  # tile_2
+           row[4] if row[4].isdigit() else None,  # tile_3
+           row[5] if row[5].isdigit() else None   # tile_4
+        )
+        db.execute_query(sql, args)
+
+def set_observation_single_SB_1D_pipeline(row, band_number):
+    if row[19] == '': # Skip empty cells
+       return
+    sql = f"""
+        UPDATE possum.observation
+        SET single_SB_1D_pipeline_band{band_number} = %s
+        WHERE name = %s;
+        """
+    args = (
+       row[19],  # single_SB_1D_pipeline
+       row[0]  # name
+    )
+    db.execute_query(sql, args)
+
+def update_tile_table():
+    """Add additional columns to the tile table if they do not exist
+       and populate data from Google Sheets
+    """
+    add_column_sql = """
+        ALTER TABLE possum.tile
+        ADD COLUMN IF NOT EXISTS "3d_pipeline_band1_status" TEXT,
+        ADD COLUMN IF NOT EXISTS "3d_pipeline_band2_status" TEXT;
+    """
+    db.execute_query(add_column_sql)
+    #POSSUM Status Sheet: Survey Tiles - Band 1: 3d_pipeline
+    ps = gc.open_by_url(status_sheet)
+    tile_sheet = ps.worksheet('Survey Tiles - Band 1')
+    tile_data = tile_sheet.get_all_values()
+    for row in tile_data[1:]:  # Skip header row
+        set_tile_3d_pipeline(row, band_number='1')
+    #POSSUM Status Sheet: Survey Tiles - Band 2: 3d_pipeline
+    tile_sheet = ps.worksheet('Survey Tiles - Band 2')
+    tile_data = tile_sheet.get_all_values()
+    for row in tile_data[1:]:  # Skip header row
+        set_tile_3d_pipeline(row, band_number='2')
+
+def set_tile_3d_pipeline(row, band_number):
+    if row[10] == '': # Skip empty cells
+        return
+    sql = f"""
+        UPDATE possum.tile
+        SET "3d_pipeline_band{band_number}_status" = %s
+        WHERE tile = %s;
+    """
+    args = (
+       row[10],  # 3d_pipeline
+       row[0]  # tile_id
+    )
+    db.execute_query(sql, args)
+
+if __name__ == "__main__":
+    """Main function to update the tables and insert data.
+    """
+    load_dotenv(dotenv_path='config.env')
+    google_api_token = os.getenv('GOOGLE_API_TOKEN_PATH')
+    gc = gspread.service_account(filename=google_api_token)
+    validation_sheet = os.getenv('POSSUM_PIPELINE_VALIDATION_SHEET')
+    status_sheet = os.getenv('POSSUM_STATUS_SHEET')
+
     create_partial_tile_pipeline_table()
     insert_partial_tile_data()
-    
+    update_observation_table()
+    update_tile_table()
